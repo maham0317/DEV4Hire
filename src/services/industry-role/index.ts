33cebd5bc77f1a2f6301121e6e3f7dd2fae1b248
M Umer <umerkhadim16@outlook.com>
2024-03-15 20:06:59 +0500

    create Curd operation of industry Role and application and business focus api

---
+++ src/services/industry-role/index.ts
@@ -0,0 +1,41 @@
+import IndustryRoleModel from "../../interfaces/industry/industry.model";
+import api from "../ApiClient";
+
+export async function getAllIndustryRole(): Promise<any> {
+  let url = `industryrole/list`;
+  const response: any = await api.get(url);
+  return response.data;
+}
+export async function getIndustryRoleById(id: number): Promise<any> {
+  let url = `industryrole/list/${id}`;
+  const response: any = await api.get(url);
+  return response.data;
+}
+
+export async function deleteIndustryRoleById(id: number): Promise<any> {
+  let url = `industryrole/delete/${id}`;
+  const response: any = await api.delete(url);
+  return response.data;
+}
+
+export async function updateIndustryRoleById(id: number): Promise<any> {
+  let url = `industryrole/update/${id}`;
+  const response: any = await api.put(url);
+  return response.data;
+}
+
+export async function createIndustryRole(
+  args: IndustryRoleModel
+): Promise<any> {
+  let url = `industryrole/create`;
+  const response: any = await api.post(url);
+  return response.data;
+}
+
+export const industryRoleService = {
+  getAllIndustryRole,
+  getIndustryRoleById,
+  deleteIndustryRoleById,
+  updateIndustryRoleById,
+  createIndustryRole,
+};
