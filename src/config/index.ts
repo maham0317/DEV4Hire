f18a0614c7e7ebfe3852ff787060d18c0e4b5a6c
Yasir Butt <yasir@makesol.no>
2024-05-31 12:56:40 +0500

    fixed merge conflicts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,18 @@
 export const Config = {
-  API_URL: "http://developer4hire-api.makesol.no/api/",
+  API_URL: "https://dev4hire-api.codify.no/api/",
+  tokenStorageKey: 'D4HRT',
   Filter: {
     PageSize: 2,
   },
   Toast: {
     DelayInMs: 1000,
   },
+  defaultLoginValues: {
+    UserName: '',
+    Password: '',
+    ClientId: 'consoleApp',
+    Scope: 'FB175C83-9703-4DF8-977B-638D6EE14D40',
+    ClientSecret: '!Ppaelosnoc@Ci'
+  }
 };
 //7241
