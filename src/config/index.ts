a7e2a663d4e982745e8fe6aafa56f7e08bf3c862
shaheryar.babar <shaheryar.babar@codeit.pk>
2024-06-03 15:37:54 +0500

    env variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,20 @@
+console.log('env', process.env)
+
 export const Config = {
-  API_URL: "https://dev4hire-api.codify.no/api/",
-  tokenStorageKey: 'D4HRT',
-  Filter: {
-    PageSize: 2,
-  },
-  Toast: {
-    DelayInMs: 1000,
-  },
-  defaultLoginValues: {
-    UserName: '',
-    Password: '',
-    ClientId: 'consoleApp',
-    Scope: 'FB175C83-9703-4DF8-977B-638D6EE14D40',
-    ClientSecret: '!Ppaelosnoc@Ci'
-  }
+    API_URL: process.env.REACT_APP_BASE_URL,
+    SECRATE_KEY: process.env.REACT_APP_SECRATE_KEY || 'FB175C83-9703-4DF8-977B-638D6EE14D40',
+    tokenStorageKey: process.env.REACT_APP_TOKEN_KEY || 'D4HRT',
+    Filter: {
+      	PageSize: 2,
+    },
+    Toast: {
+      	DelayInMs: 1000,
+    },
+    defaultLoginValues: {
+		UserName: '',
+		Password: '',
+		ClientId: 'consoleApp',
+		Scope: 'FB175C83-9703-4DF8-977B-638D6EE14D40',
+		ClientSecret: '!Ppaelosnoc@Ci'
+    }
 };
-//7241
